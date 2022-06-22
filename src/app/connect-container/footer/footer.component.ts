7b3318e655c9b2fc36c330fc9540e7d015a92894
Sovlo <94002236+Sovlo1@users.noreply.github.com>
2022-06-22 07:10:29 +0200

    Updated responsiveness for mobile

---
+++ src/app/connect-container/footer/footer.component.ts
@@ -0,0 +1,24 @@
+import { Component, HostListener, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss'],
+})
+export class FooterComponent implements OnInit {
+  public currentWindowWidth!: number;
+
+  @HostListener('window:resize')
+  onResize() {
+    this.currentWindowWidth = window.innerWidth;
+  }
+
+  constructor(private auth: AuthService, public router: Router) {}
+
+  ngOnInit(): void {
+    this.currentWindowWidth = window.innerWidth;
+    console.log(this.currentWindowWidth);
+  }
+}
--- src/app/main-container/user-info/user-info.component.ts
+++ src/app/main-container/user-info/user-info.component.ts
@@ -1,4 +1,5 @@
 import { Component, HostListener, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { User } from 'src/app/models/user.model';
 import { AuthService } from 'src/app/services/auth.service';
@@ -13,12 +14,36 @@ export class UserInfoComponent implements OnInit {
   public users$!: Observable<User>;
   public id?: string | null;
   public userInfos!: User;
+  public auth$!: Observable<boolean>;
+  public currentWindowWidth!: number;
 
-  constructor(private users: UsersService, private auth: AuthService) {}
+  constructor(
+    private users: UsersService,
+    private auth: AuthService,
+    public router: Router
+  ) {}
+
+  @HostListener('window:resize')
+  onResize() {
+    this.currentWindowWidth = window.innerWidth;
+  }
 
   ngOnInit(): void {
+    this.auth$ = this.auth.auth$;
     this.users$ = this.users.users$;
+    this.currentWindowWidth = window.innerWidth;
+    console.log(this.currentWindowWidth);
     this.id = this.auth.getUserId();
     this.users.getUserInfos(this.id!).subscribe();
+  }
+
+  logout() {
+    this.auth.logout();
+    this.router.navigate(['../login']);
+  }
+
+  checkProfile() {
+    this.id = this.auth.getUserId();
+    this.router.navigate(['./profile/' + this.id]);
   }
 }
